66f594deb3f5985f898524d1bb48fd7f60760f23
smk-141510228 <smk-141510228>
2017-02-22 16:59:57 +0700

    Hari Kedua Ujikom

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+
+        <div class="col-10-0">
+            <div class=" panel-default">
+           
+                 <div class="panel-heading">SELAMAT DATANG DI APLIKASI PENGGAJIAN</div>
+   
+    </div>
+</div>
+@endsection
